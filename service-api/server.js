6744ece880356d43fd73fa5b8cef1b4faf31dca1
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-09-24 11:08:55 +0700

    first commit

---
+++ service-api/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan')
+const cors = require('cors');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'src', 'uploads')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
+app.use(cors());
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
